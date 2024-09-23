fe15f2677d86ca01e912fd906c0c4e49f1b81408
Romnick Sarmiento <135184315+s4rmier@users.noreply.github.com>
2024-09-23 17:13:15 +0800

    -404 -dynamic routing updated

--- src/main.js
+++ src/main.js
@@ -5,13 +5,19 @@ import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
 import TeamMembers from './components/teams/TeamMembers.vue';
+import NotFound from './components/nav/NotFound.vue';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
+      path: '/',
+      redirect: '/teams',
+    },
+    {
       path: '/teams',
       component: TeamsList,
+      // alias: '/' this allows this component to be rendered when path is = to '/'
     },
     {
       path: '/users',
@@ -19,7 +25,7 @@ const router = createRouter({
     },
     // {
     //   path: '/teams/new',
-    //   //   order matters. teams/new must come first before teams/:teamId
+    //   //   order matters. teams/new must come first before teams/:teamId because teams/new can be interpreted as /:teamId
     // },
     {
       path: '/teams/:teamId',
@@ -27,6 +33,8 @@ const router = createRouter({
       component: TeamMembers,
       props: true,
     },
+    { path: '/404', component: NotFound },
+    { path: '/:notFound(.*)', redirect: '/404' }, //catch all url/url parameters.. can send to page not found
   ],
   linkActiveClass: 'active', //this can be used for styling purposes. by default, the class assigned to an active route is 'router-link-active'. linkActiveClass is a way to change the default active class.
 });
